817d842af038080ea1738581253ffa08687b9d2d
leopii98 <rippergames98@gmail.com>
2025-11-15 10:26:01 +0100

    add-final/payout

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
-document.getElementById("data-control").addEventListener("click", function () {
+document.getElementById("data-control").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // Nome
     let push = document.getElementById("control-name").value;
-    console.log(push);
     document.getElementById("result-name").innerText = push;
-   
-   
-   
-   
-   
-    let pushKm = document.getElementById("control-km").value;
-    console.log(pushKm);
+    
+    // Dati da elaborare
+    const kmPercorrere = Number(document.getElementById("control-km").value);
+    const etàPasseggero = document.getElementById("control-age").value;
+
+    let costoKm = kmPercorrere * 0.21;
+
+    if (etàPasseggero === "tariffa-young") {
+        costoKm = costoKm * 0.8; // sconto 20%
+    } else if (etàPasseggero === "tariffa-anziani") {
+        costoKm = costoKm * 0.6; // sconto 40%
+    }
+
+    costoKm = costoKm.toFixed(2);
+    document.getElementById("result-pay").innerText = (costoKm + " €");
 });
-
-
-//Dati da elaborare
-//const kmPercorrere = Number(km);
-//const etàPasseggero = Number(età);
-
-//let costoKm = kmPercorrere * 0.21;
-//if (etàPasseggero < 18) {
-//costoKm = costoKm * 0.8; //sconto 20%
-//}
-//else if(etàPasseggero > 65) {
-//costoKm = costoKm * 0.6; //sconto 40%
-//}
-
-//costoKm = costoKm.toFixed(2); //Arrotondare in decimali
-
-//alert("Il prezzo del tuo biglietto è: € " + costoKm); //Costo del Biglietto!
